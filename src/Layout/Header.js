423a2ee67ab85feb3dcdcf68be6324c10c43ce60
Gopala Indira <a.gopala.indira@accenture.com>
2022-09-24 00:46:09 +0530

    add Axious Library and created hook funcation to all APIS and refactored Footer and header

---
+++ src/Layout/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export let Header = () => {  
+    return (
+        <div style={{width:"100%","text-align": "center", "border": "1px solid red"}}>
+            <h1>------Header------</h1>
+        </div>
+    );
+}
--- src/Router/PublicRoutes.js
+++ src/Router/PublicRoutes.js
@@ -8,7 +8,7 @@ function PublicRoutes() {
     return (
         <Routes>
             <Route path="/login" element={<LoginLayout />} >
-                <Route index element={<Home />} />
+                <Route index element={<LoginLayout />} />
             </Route>
         </Routes>
     )
