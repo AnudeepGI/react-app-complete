6fbbdf048110d1c4cfc85785594001828cd29fa4
Gopala Indira <a.gopala.indira@accenture.com>
2022-09-26 21:48:41 +0530

    implemnted errror reporting tool - sentry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import ErrorBoundary from "./ErrorBoundary";
 import Router from "./Router";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "", // removed URL for security issues get URL from Sentry website with ****
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 function App() {
   return (
     <ErrorBoundary>
